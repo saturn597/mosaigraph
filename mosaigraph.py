a38eb78db3232250eb810b320e1d3c160a447bc8
Me <Me>
2015-03-16 09:17:51 -0400

    Now can match each piece in image with an image in database
    So can now make actual photomosaics.
    
    Also, added some command line option processing so we can easily
    test different aspects of the program.

--- mosaigraph.py
+++ mosaigraph.py
@@ -1,14 +1,23 @@
 from PIL import Image
 
 import dataset
+import getopt
+import math
 import random
-import re
 import os
+import re
+import sys
 
 imagePath = './images'
 dbname = 'sqlite:///data.db'
-dbtable = 'image'
+db = dataset.connect(dbname)
+dbtable = db['image']
 
+usage_string = """
+Usage: 
+-c: specify a directory to catalog
+-g: specify a "group" of images
+"""
 def getColorAvg(img):
     # returns the average color of all pixels in an rgb image
     width, height = img.size
@@ -83,19 +92,34 @@ def divideImage(img, pieceWidth, pieceHeight):
         pieceData.append({ 'x': piece[0], 'y': piece[1], 'image': cropped })
     return pieceData
 
-def makeMosaigraph(img, horizDivs, vertDivs):
+def makeMosaigraph(img, horizDivs, vertDivs, group):
     newImg = Image.new(img.mode, img.size)
     width, height = img.size
     pieceWidth = width / horizDivs
     pieceHeight = height / vertDivs
     pieces = divideImage(img, pieceWidth, pieceHeight)
+    length = len(pieces)
+    currentNum = 0
     for piece in pieces:
-        r, g, b = getColorAvg2(getSomePixels(piece['image'], 1))
-        addOn = Image.new('RGB', (pieceWidth, pieceHeight), (r, g, b))
+        rgb = getColorAvg2(getSomePixels(piece['image'], 1))
+        #addOn = Image.new('RGB', (pieceWidth, pieceHeight), (rgb)) #this line would just paste the exact color in
+        addOn = makeProportional(Image.open(findMatch(rgb, group))).resize((pieceWidth, pieceHeight))
         newImg.paste(addOn, (piece['x'], piece['y']))
+        currentNum += 1
+        print "Currently on piece " + str(currentNum) + " out of " + str(length)
     return newImg
-        
+ 
+def findMatch(rgb, group):
+    path_to_closest = ""
+    least_distance = math.sqrt(255**2 + 255 ** 2 + 255 ** 2)
+    for image in dbtable.find(group = group):
+        distance = math.sqrt((image['r'] - rgb[0])**2 + (image['g'] - rgb[1])**2 + (image['b'] - rgb[2])**2)
+        if distance < least_distance:
+            path_to_closest = image['path']
+            least_distance = distance
     
+    return path_to_closest
+
 #toShow.paste(cropped, (0, 0))  # for some reason passing None as the box results in an error ("images do not match") - docs say these are the same
 
 def makeProportional(img, ratio = 1):
@@ -114,21 +138,49 @@ def makeProportional(img, ratio = 1):
         return img
 
 
-def catalogImages(imagePath):
-    db = dataset.connect(dbname)
-    table = db[dbtable]
+def catalogImages(imagePath, group):
     for filename in getImages(imagePath):
         fullPath = os.path.join(imagePath, filename)
         image = makeProportional(Image.open(fullPath))
-        if not table.find_one(path = fullPath):  # only do all the calculations if we haven't already checked this file
-            avg = getColorAvg(image)
-            table.insert(dict(path = fullPath, r = avg[0], g = avg[1], b = avg[2]))
-            print filename + ' ' + str(getColorAvg(image))
+        if not dbtable.find_one(path = fullPath):  # only do all the calculations if we haven't already checked this file
+            avg = getColorAvg2(getSomePixels(image, 100))
+            print fullPath + " : " + str(avg)
+            dbtable.insert(dict(path = fullPath, r = avg[0], g = avg[1], b = avg[2], group = group))
+
+
+def main(argv):
+    catalog_directory = None
+    group = None
+    image_path = None
+
+    try:
+        opts, args = getopt.getopt(argv, "c:g:i:", ["catalog=", "group=", "image="])
+    except getopt.GetoptError:
+        print usage_string
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-c", "--catalog"):
+            catalog_directory = arg
+        elif opt in ("-g", "--group"):
+            group = arg
+        elif opt in ("-i", "--imagePath"):
+            image_path = arg
+            
+    if catalog_directory:
+        catalogImages(catalog_directory, group)
+    else:
+        i = makeMosaigraph(Image.open(image_path), 20, 30, group)
+        i.show()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
 
 
 
+"""
 img = Image.open('./images/test3.jpg')
 makeMosaigraph(img, 10, 10).show()
 makeMosaigraph(img, 50, 50).show()
 makeMosaigraph(img, 100, 100).show()
 makeMosaigraph(img, 300, 300).show()
+"""
