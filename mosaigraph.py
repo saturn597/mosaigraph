1150f0eed0e953d61d3d8a2325724d9b2b1dd7dc
Jeremy <saturn5971@gmail.com>
2015-06-24 00:30:46 -0400

    Removed unused functions

--- mosaigraph.py
+++ mosaigraph.py
@@ -137,39 +137,6 @@ class RgbMatcher(Matcher):
         for candidate in self.candidates:
             yield (candidate['r'] - rgb['r'])**2 + (candidate['g'] - rgb['g'])**2 + (candidate['b'] - rgb['b'])**2
 
-
-class ImageData(object):
-    def __init__(self, path = None, image = None, x = None, y = None, rgb = None):
-        self.path = path
-        self.image = image
-        self.rgb = rgb
-        self.x = x
-        self.y = y
-        self.closest_distance = None
-        self.match = None
-
-    def get_image(self):
-        if self.image:
-            return self.image
-
-        try:
-            self.image = make_proportional(Image.open(self.path).convert(mode = "RGB"))
-        except (IOError, struct.error):
-            # IOError is usually because the file isn't an image file.
-            # TODO: look into what's causing struct.error
-            pass
-        
-        return self.image
-
-    def get_rgb(self):
-        if self.rgb:
-            return self.rgb
-
-        avg = get_color_avg(get_n_pixels(self.get_image(), 300))
-        self.rgb = (avg['r'], avg['g'], avg['b'])  
-
-        return self.rgb
-
 def collect_candidates(dbtable, paths, rgb_needed, pixels_needed, sampler):
     # work on ability to use paths AND db
     # work on what happens if a db has rgb but not pixels and vice versa
@@ -206,100 +173,6 @@ def collect_candidates(dbtable, paths, rgb_needed, pixels_needed, sampler):
 
     print("")
     return candidates
-
-def collect_candidates2(dbtable, paths = [], rgb_needed = False):
-    candidates = collect_candidates(dbtable, paths = [], rgb_needed = False)
-
-    for candidate in candidates:
-        yield ImageData(path = candidate['path'], rgb = (candidate['r'], candidate['g'], candidate['b']))
-
-def get_matches(images, candidates, pixelwise = False, unique = False):
-
-    # add pixelwise
-    if not pixelwise and not unique:
-        kdtree = spatial.KDTree([item.get_rgb() for item in candidates])
-    
-    for image in images:
-        if unique:
-            distance, match_index = find_match_linear2(image.get_rgb(), candidates)
-            match = candidates[match_index]
-        else:
-            distance, match_index = kdtree.query(image.get_rgb())
-            match = candidates[match_index]
-
-        image.closest_distance = distance
-        image.match = match
-
-        if not match.closest_distance or match.closest_distance and distance <= match.closest_distance:
-            match.closest_distance = distance
-            match.match = image
-
-
-def get_piece(image, edge_length, x, y):
-    new_image = image.crop((x * edge_length, y * edge_length, (x + 1) * edge_length, (y + 1) * edge_length))  
-    return ImageData(image = new_image, x = x, y = y)
-
-def make_mosaigraph2(img, candidates, num_pieces, edge_length = 100, unique = False, randomize_order = False, pixelwise = False):
-    # makes a photomosaic, returning a PIL.Image representing it, which can then be displayed, saved, etc, and a dict describing the images used and where
-
-    # TODO: subjective observation, randomize_order seems to make it slower. Is this correct?
-    # TODO: replace incoming img with ImageData
-
-    width, height = img.size
-        
-    edge_length = get_best_edge_length(width, height, num_pieces)
-    numX = width // edge_length
-    numY = height // edge_length
-    num_pieces = numX * numY
-    print("Dividing mosaic into {} pieces".format(num_pieces))
-
-    result_image = Image.new(img.mode, (width // edge_length * edge_length, height // edge_length * edge_length))
-
-    current_num = 0
-
-    piece_ids = [get_piece(img, edge_length, x, y) for x in range(0, numX) for y in range(0, numY)]
-         
-    # if we have a limited pool of images to pick from, pieces of the mosaic added later are likely to look
-    # worse than ones added earlier, as we run out of good matches. Offering option to randomize the order in 
-    # which we add pieces to the mosaic so that one region doesn't look better/worse than another. Not 
-    # confident this is necessarily better - seems to lead to an image that's equally bad all over. 
-    if randomize_order:
-        random.shuffle(piece_ids)
-   
-    paths = []
-    passnum = 0
-
-    if unique:
-        undone_pieces = [piece for piece in piece_ids if not piece.match]
-        while undone_pieces:
-            passnum += 1
-            print "beginning pass: {} images remaining".format(len(undone_pieces))
-            get_matches(candidates, undone_pieces, pixelwise, unique)
-            matches = set([piece.match for piece in piece_ids if piece.match])
-            undone_pieces = [piece for piece in piece_ids if not piece.match]
-            candidates = [candidate for candidate in candidates if candidate not in matches]
-    else:
-        get_matches(piece_ids, candidates, pixelwise, unique)
-
-    def prep_image(image):
-        proportional = make_proportional(image.get_image())
-        return proportional.resize((edge_length, edge_length))
-    
-    print piece_ids
-    for piece in piece_ids:
-        new_piece = piece.match
-        try: 
-            result_image.paste(prep_image(new_piece), (piece.x * edge_length, piece.y * edge_length))
-        except:
-            continue
-        paths.append({ 'x': piece.x, 'y': piece.y, 'path': new_piece.path })
-        current_num += 1  # maybe use enumerate in for instead
-        sys.stdout.write("\rCompleted piece " + str(current_num) + " out of " + str(num_pieces))
-        sys.stdout.flush()
-    print("")
-
-    return result_image, paths
-
 
 def make_mosaigraph(img, num_pieces, matcher, edge_length = 100, randomize_order = False, unique = False):
     # makes a photomosaic, returning a PIL.Image representing it, which can then be displayed, saved, etc, and a dict describing the images used and where
@@ -366,93 +239,6 @@ def make_mosaigraph(img, num_pieces, matcher, edge_length = 100, randomize_order
 
     print("")
     return result_image, log 
-
-
-def find_match(original, candidates, comparison_fn):
-    closest_image = None
-    least_distance = None
-
-    for n, distance in enumerate(comparison_fn(original, candidates)):
-        if not least_distance or distance < least_distance:
-            closest_index = n
-            least_distance = distance
-
-    return closest_index
-
-def find_match_orig(original, candidates, comparison_fn):
-      # maybe combine with find_match_linear
-      closest_image = None
-      least_distance = None
-
-      for n, candidate in enumerate(candidates):
-          distance = comparison_fn(original, candidate['path'])
-          if not least_distance or distance < least_distance:
-                closest_index = n
-                least_distance = distance
-
-      return closest_index
-
-def compare_rgb(original, candidates):
-    rgb = get_color_avg(get_n_pixels(original, 300))
-    for candidate in candidates:
-        yield (candidate['r'] - rgb['r'])**2 + (candidate['g'] - rgb['g'])**2 + (candidate['b'] - rgb['b'])**2
-
-def compare_pixelwise2(img, candidates, n = 300):
-    width, height = img.size
-
-    global pts_to_sample  # Need these points to be consistent between calls, or data from the image_cache will be bogus
-    if not pts_to_sample:
-        pts_to_sample = [(random.randint(0, width - 1), random.randint(0, height - 1)) for i in range(n)]
-
-    pixels = [img.getpixel((x, y)) for x, y in pts_to_sample]
-
-    for candidate in candidates:
-        path = candidate['path']
-        if path in image_cache:  # I think there may be a pythonic way to avoid this idiom; also, make this its own function
-            pixels2 = image_cache[path]
-        else:
-            try:
-                img2 = make_proportional(Image.open(path), width / height).resize((width, height)).convert(mode = 'RGB')  # maybe avoid actually resizing/reproportioning the image
-            except IOError:
-                print('Couldn\'t process file {} as image'.format(path))
-                continue
-            pixels2 = [img2.getpixel((x, y)) for x, y in pts_to_sample]
-            image_cache[path] = pixels2
-
-        diff = 0
-        
-        for pixel1, pixel2 in zip(pixels, pixels2):
-            diff += abs(pixel1[0] - pixel2[0]) + abs(pixel1[1] - pixel2[1]) + abs(pixel1[2] - pixel2[2])
-        yield diff
-
-
-def compare_pixelwise(img, img2 = None, path = None, n = 300):
-    # combine with compare_pixelwise2
-
-    width, height = img.size
-
-    if not img2:
-        if not path:
-            raise  # TODO: Make this better
-        img2 = Image.open(path)
-
-    if path and path in image_cache:  # I think there may be a pythonic way to avoid this idiom; also, make this its own function
-        img2 = image_cache[path] 
-    else:
-        img2 = make_proportional(img2, width / height).resize((width, height)).convert(mode = 'RGB')  # maybe avoid actually resizing/reproportioning the image
-        image_cache[path] = img2
-
-    pixels = [(random.randint(0, width - 1), random.randint(0, height - 1)) for i in range(n)]
-    
-    diff = 0
-    
-    for pixel in pixels:
-        pixel1 = img.getpixel((pixel[0], pixel[1]))
-        pixel2 = img2.getpixel((pixel[0], pixel[1]))  # probably don't need to sample the original image repeatedly
-        diff1 = abs(pixel1[0] - pixel2[0]) + abs(pixel1[1] - pixel2[1]) + abs(pixel1[2] - pixel2[2])
-        diff += diff1
-  
-    return diff
 
 def get_color_avg(pixels):
     # get the average color in a iterable of rgb pixels
@@ -594,6 +380,7 @@ def process_image(path, rgb_needed, pixels_needed, pixelwise_sampler = None):
        d['pixels'] = pixelwise_sampler.sample_image(image)
     
     if rgb_needed:
+        # since pixels_needed already samples a bunch of pixels, maybe we could avoid redoing the sampling in cases where we also need an average
         avg = get_color_avg(get_n_pixels(image, 300))
         d.update(avg)
 
@@ -649,8 +436,6 @@ def main(argv):
                 sys.exit(0) 
         
         print("Making mosaic out of {}...".format(args.filename))
-        
-        # switch to make_mosaigraph2 and collect_candidates2 to attempt the alternative strategy - using my own wrapper object for images, and using alternative "uniqueness" strategy
 
         sampler = PixelwiseSampler(300)
         if sample_info:
